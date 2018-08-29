88edea3bb8b3f539a9bd40491474ffc049682372
ajetty <22653345+ajetty@users.noreply.github.com>
2018-08-29 06:02:44 -0700

    Add files via upload

--- GameController.java
+++ GameController.java
@@ -27,57 +27,61 @@ public class GameController {
 		//long = G1G2G3G4G5G60 F1F2F3F4F5F60 E1E2E3E4E5E60 D1D2D3D4D5D60 C1C2C3C4C5C60 B1B2B3B4B5B60 A1A2A3A4A5A60 0000000 0000000 0
 			
 			
-			gui.playerMoveProperty().addListener(GUIListener = new ChangeListener<Object>() {
-				@Override public void changed(ObservableValue<?> o, Object oldVal, Object newVal) {
-					playerOne.setIsTurnProperty();
-					playerTwo.setIsTurnProperty();
-				}
-			});
-			
-			playerOne.isTurnProperty().addListener(playerOneListener = new ChangeListener<Object>() {
-				@Override public void changed(ObservableValue<?> o, Object oldVal, Object newVal) {
-					if(playerOne.isTurnProperty().getValue() == false) {
-						long binaryValue = (long) Math.pow(2, gui.playerMoveProperty().get());
-						
-						System.out.println("Binary Value: " + binaryValue + " " + Long.toBinaryString(binaryValue));
-						
-						long playerMove = playerOne.movePlayer(binaryValue, playerOne.getBitBoard(), playerTwo.getBitBoard());
-						
-						if(playerMove > 0) {
-							playerOne.setBitBoard(playerMove);
-							System.out.println("Player one: " + Long.toBinaryString(playerOne.getBitBoard()));
-							gui.updateBoard(binaryValue, 1);
-							gameOver = playerOne.checkWin(playerOne.getBitBoard());
-						}
-					}
+		gui.playerMoveProperty().addListener(GUIListener = new ChangeListener<Object>() {
+			@Override public void changed(ObservableValue<?> o, Object oldVal, Object newVal) {
+				playerOne.setIsTurnProperty();
+				playerTwo.setIsTurnProperty();
+			}
+		});
+		
+		playerOne.isTurnProperty().addListener(playerOneListener = new ChangeListener<Object>() {
+			@Override public void changed(ObservableValue<?> o, Object oldVal, Object newVal) {
+				if(playerOne.isTurnProperty().getValue() == false) {
+					long binaryValue = (long) Math.pow(2, gui.playerMoveProperty().get());
 					
-					if(gameOver == true) {
-						endGame();
-					}
-				}
-			});
-			
-			playerTwo.isTurnProperty().addListener(playerTwoListener = new ChangeListener<Object>() {
-				@Override public void changed(ObservableValue<?> o, Object oldVal, Object newVal) {
-					if(playerTwo.isTurnProperty().getValue() == false) {
-						long binaryValue = (long) Math.pow(2, gui.playerMoveProperty().get());
-						
-						long playerMove = playerOne.movePlayer(binaryValue, playerTwo.getBitBoard(), playerOne.getBitBoard());
-						
-						if(playerMove > 0) {
-							playerTwo.setBitBoard(playerMove);
-							System.out.println("Player two: " + Long.toBinaryString(playerTwo.getBitBoard()));
-							gui.updateBoard(binaryValue, 2);
-							gameOver = playerTwo.checkWin(playerTwo.getBitBoard());
-						}
-						 
-					}
+					//System.out.println("Binary Value: " + binaryValue + " " + Long.toBinaryString(binaryValue));
 					
-					if(gameOver == true) {
-						endGame();
+					long playerMove = playerOne.movePlayer(binaryValue, playerOne.getBitBoard(), playerTwo.getBitBoard());
+					
+					System.out.println(playerMove);
+					
+					if(playerMove > 0) {
+						playerOne.setBitBoard(playerMove);
+						System.out.println("Player one: " + Long.toBinaryString(playerOne.getBitBoard()));
+						gui.updateBoard(playerMove, 1);
+						gameOver = playerOne.checkWin(playerOne.getBitBoard());
 					}
 				}
-			});
+				
+				if(gameOver == true) {
+					endGame();
+				}
+			}
+		});
+		
+		playerTwo.isTurnProperty().addListener(playerTwoListener = new ChangeListener<Object>() {
+			@Override public void changed(ObservableValue<?> o, Object oldVal, Object newVal) {
+				if(playerTwo.isTurnProperty().getValue() == false) {
+					long binaryValue = (long) Math.pow(2, gui.playerMoveProperty().get());
+					
+					long playerMove = playerTwo.movePlayer(binaryValue, playerTwo.getBitBoard(), playerOne.getBitBoard());
+					
+					System.out.println(playerMove);
+					
+					if(playerMove > 0) {
+						playerTwo.setBitBoard(playerMove);
+						System.out.println("Player two: " + Long.toBinaryString(playerTwo.getBitBoard()));
+						gui.updateBoard(playerMove, 2);
+						gameOver = playerTwo.checkWin(playerTwo.getBitBoard());
+					}
+					 
+				}
+				
+				if(gameOver == true) {
+					endGame();
+				}
+			}
+		});
 		
 	}
 	
